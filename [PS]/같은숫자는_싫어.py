5fe0a6713015a60e3efed489184ab51a14cae509
jiyun kim <jiy00n1yun@gmail.com>
2024-02-07 21:24:26 +0900

    first commit

---
+++ [PS]/같은숫자는_싫어.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    answer = []
+    
+    #음수를 우선 할당하여 첫번째 요소가 무조건 추가되도록 함
+    last =-1
+    
+    for a in arr:
+        #이전 값과 같지 않을 때만 a를 추가함
+        if a != last:
+            answer.append(a)
+            #다음 비교를 위해 값을 last 에 넣어줌
+        last=a
+    
+    return answer
+
+print(solution([1,1,2,3,1]))
