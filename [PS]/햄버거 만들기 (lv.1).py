fac5e44eed72d2450c7a4670323236b4ce33a77a
jiyun kim <jiy00n1yun@gmail.com>
2024-02-12 13:31:52 +0900

    .

--- [PS]/햄버거 만들기 (lv.1).py
+++ [PS]/햄버거 만들기 (lv.1).py
@@ -1,1 +1,17 @@
-..
+def solution(ingredient):
+    answer = 0 # 햄버거의 개수
+    
+    h = [] # 햄버거 재료 배열
+    for i in ingredient:
+        h.append(i) # 재료를 햄버거 배열에 추가
+        #print(h)
+        if h[-4:]==[1,2,3,1]:
+            answer+=1
+            #햄버거의 개수 추가
+            for j in range(4):
+                h.pop() #원소 4개 제외
+                #print(h)
+                
+    return answer
+
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
