47bf1bdafd1bd0d3fa1f780eea0466510fdc802e
Jiyun Kim <145686332+Yuncodeinside@users.noreply.github.com>
2024-07-02 17:20:18 +0900

    0702

--- [PS]/Stack_queue/주식가격.py
+++ [PS]/Stack_queue/주식가격.py
@@ -1,16 +1,17 @@
 def solution(prices):
-    n = len(prices)
-    #리스트 초기화
-    answer = [0]*n
-    stack = []
+    n = len(prices) # 주식 가격의 길이
+    answer = [0]*n  # 배열 초기화
+    stack = [] # 인덱스 저장
     
     for i in range(n):
-        
+        # 현재 가격이 스택의 마지막 가격보다 낮을 경우
         while stack and prices[i] < prices[stack[-1]]:
-            j = stack.pop()
-            answer[j] = i-j
+            j = stack.pop() # 스택에서 인덱스를 꺼냄
+            answer[j] = i-j # 남은 시간 계산
+        #stack이 비어 
         stack.append(i)
-        
+    
+    #스택에 남아 있는 인덱스
     while stack:
         j = stack.pop()
         answer[j] = n-1-j
